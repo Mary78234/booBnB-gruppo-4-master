4b9ff4ba30d0e4e22b36379515f487cff4a98a92
malesani <alejandroMalesani@hotmail.com>
2021-07-19 14:48:39 +0200

    config

--- public/js/app.js
+++ public/js/app.js
@@ -18556,8 +18556,8 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/macbook/Documents/boolbnb-gruppo-4-master/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/macbook/Documents/boolbnb-gruppo-4-master/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Alejandro Malesani\Documents\booBnB-gruppo-4-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Alejandro Malesani\Documents\booBnB-gruppo-4-master\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
