9181fd6e919ecb3787652bbdb20a5dce08bd01b8
AleColumn99 <alecolumn.99@gmail.com>
2021-07-20 16:41:39 +0200

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,178 @@
+<footer>
+
+  <div class="container">
+
+    <div class="footer-top row">
+
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <h3>ABOUT</h3>
+        <ul>
+          <li>
+            How Airbnb works
+          </li>
+          <li>
+            Newsroom
+          </li>
+          <li>
+            Airbnb 2021
+          </li>
+          <li>
+            Investors
+          </li>
+          <li>
+            Airbnb Plus
+          </li>
+          <li>
+            Airbnb Luxe
+          </li>
+          <li>
+            HotelTonight
+          </li>
+          <li>
+            Airbnb for Work
+          </li>
+          <li>
+            Made possible by Hosts
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>
+            Founders' Letter
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <h3>ABOUT</h3>
+        <ul>
+          <li>
+            How Airbnb works
+          </li>
+          <li>
+            Newsroom
+          </li>
+          <li>
+            Airbnb 2021
+          </li>
+          <li>
+            Investors
+          </li>
+          <li>
+            Airbnb Plus
+          </li>
+          <li>
+            Airbnb Luxe
+          </li>
+          <li>
+            HotelTonight
+          </li>
+          <li>
+            Airbnb for Work
+          </li>
+          <li>
+            Made possible by Hosts
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>
+            Founders' Letter
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <h3>ABOUT</h3>
+        <ul>
+          <li>
+            How Airbnb works
+          </li>
+          <li>
+            Newsroom
+          </li>
+          <li>
+            Airbnb 2021
+          </li>
+          <li>
+            Investors
+          </li>
+          <li>
+            Airbnb Plus
+          </li>
+          <li>
+            Airbnb Luxe
+          </li>
+          <li>
+            HotelTonight
+          </li>
+          <li>
+            Airbnb for Work
+          </li>
+          <li>
+            Made possible by Hosts
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>
+            Founders' Letter
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <h3>ABOUT</h3>
+        <ul>
+          <li>
+            How Airbnb works
+          </li>
+          <li>
+            Newsroom
+          </li>
+          <li>
+            Airbnb 2021
+          </li>
+          <li>
+            Investors
+          </li>
+          <li>
+            Airbnb Plus
+          </li>
+          <li>
+            Airbnb Luxe
+          </li>
+          <li>
+            HotelTonight
+          </li>
+          <li>
+            Airbnb for Work
+          </li>
+          <li>
+            Made possible by Hosts
+          </li>
+          <li>
+            Careers
+          </li>
+          <li>
+            Founders' Letter
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom row">
+
+      <div class="footer-bottom-left col-sm-6 col-xs-12">
+        &copy; 2021 BoolBnB, Inc. · Privacy · Terms · Sitemap
+      </div>
+
+      <div class="footer-bottom-right col-sm-6 col-xs-12 text-right">
+        <i class="fas fa-globe"></i> Italiano (IT) · € EUR <i class="fab fa-facebook-f"></i> <i class="fab fa-twitter"></i> <i class="fab fa-instagram"></i>
+
+    </div>
+
+  </div>
+
+</footer>
